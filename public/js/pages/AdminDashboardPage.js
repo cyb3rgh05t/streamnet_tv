97e72fba39a579bfe2d030e527f7666b5239feda
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2026-06-10 11:00:40 +0200

    ui update

--- public/js/pages/AdminDashboardPage.js
+++ public/js/pages/AdminDashboardPage.js
@@ -455,10 +455,10 @@ class AdminDashboardPage {
         const p2 = linePoints[i + 1];
         const p3 = linePoints[i + 2] || p2;
 
-        const cp1x = p1.x + (p2.x - p0.x) / 6;
-        const cp1y = p1.y + (p2.y - p0.y) / 6;
-        const cp2x = p2.x - (p3.x - p1.x) / 6;
-        const cp2y = p2.y - (p3.y - p1.y) / 6;
+        const cp1x = p1.x + (p2.x - p0.x) / 5;
+        const cp1y = p1.y + (p2.y - p0.y) / 5;
+        const cp2x = p2.x - (p3.x - p1.x) / 5;
+        const cp2y = p2.y - (p3.y - p1.y) / 5;
 
         d += ` C ${cp1x.toFixed(2)} ${cp1y.toFixed(2)}, ${cp2x.toFixed(2)} ${cp2y.toFixed(2)}, ${p2.x.toFixed(2)} ${p2.y.toFixed(2)}`;
       }
